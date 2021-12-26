311b691884fe2e934d42fb3e930bcfbac75057b7
Erkam <erkam.bozan@gmail.com>
2021-12-27 02:40:01 +0300

    setup proxy

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const createProxyMiddleware = require('http-proxy-middleware');
+
+module.exports = function (app) {
+    app.use(
+        '/hat/*',
+        createProxyMiddleware({
+            target: 'http://hat-service:8080',
+            changeOrigin: true,
+        })
+    );
+};
